a58d38cad0299c7ebc5bc1b87f689e757f595404
vaishu1205 <119730123+vaishu1205@users.noreply.github.com>
2024-01-09 14:43:47 +0530

    import java.util.Scanner; public class Inputout {     public static void main(String[] args)     {         Scanner read= new Scanner(System.in);         String a=read.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Inputout
+{
+    public static void main(String[] args)
+    {
+        Scanner read= new Scanner(System.in);
+        String a=read.nextLine();
+        System.out.println(a);
+    }
+}
