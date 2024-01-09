f092e5a516ba7d6498e6b33c8c12c149e01a05a6
vaishu1205 <119730123+vaishu1205@users.noreply.github.com>
2024-01-09 14:34:22 +0530

    import java.util.Scanner; public class Divisionumber {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int a= read.nextInt();         int b= read.nextInt();         System.out.println(a/b);     } }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Divisionumber
+{ 
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int a= read.nextInt();
+        int b= read.nextInt();
+        System.out.println(a/b);
+    }
+}
