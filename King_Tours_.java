0716b2b10018dfb5c965f54d9ae22e4c1af6603c
vaishu1205 <119730123+vaishu1205@users.noreply.github.com>
2024-01-18 22:40:18 +0530

    import java.util.Scanner; public class King {     public static void main(String[] args)     {         Scanner read =  new Scanner(System.in);         int n = read.nextInt();         int m = read.nextInt();         System.out.println((n*5)+(m*7));     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class King
+{
+    public static void main(String[] args)
+    {
+        Scanner read =  new Scanner(System.in);
+        int n = read.nextInt();
+        int m = read.nextInt();
+        System.out.println((n*5)+(m*7));
+    }
+}
