0d5b1d74c7453f8b628a0b34c540a4b1b6139c28
vaishu1205 <119730123+vaishu1205@users.noreply.github.com>
2024-01-18 22:51:57 +0530

    import java.util.Scanner; public class Reqpnts {     public static void main(String[] args)     {         Scanner read = new Scanner(System.in);         int a = read.nextInt();         int b = read.nextInt();         System.out.println(b/a);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Reqpnts
+{
+    public static void main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+        int b = read.nextInt();
+        System.out.println(b/a);
+    }
+}
