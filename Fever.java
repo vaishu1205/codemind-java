72662b8bda2be226daec03a0700a7e5ba6f1a09a
vaishu1205 <119730123+vaishu1205@users.noreply.github.com>
2024-03-10 11:28:29 +0530

    import java.util.Scanner; public class Fever {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         if(x>98)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Fever.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Fever
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x>98)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
